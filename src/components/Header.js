858c6593c04184ed55510e6e6722985d8a6eb6db
matin-binary <matin@binary.com>
2021-08-18 23:46:07 +0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+  <div className="App-header">
+    <h1>React Hangman Game</h1>
+    <p>Find the hidden word to save the man!</p>
+  </div>
+);
+
+export default Header;
